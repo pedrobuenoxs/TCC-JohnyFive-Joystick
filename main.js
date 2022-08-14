527008b7051ef0c18b770c02dc3530e45148cdad
pedrobuenoxs <pedrohbueno02@gmail.com>
2022-08-14 20:31:30 -0300

    add main

--- main.js
+++ main.js
@@ -1,7 +1,17 @@
 const { Accelerometer, Board } = require("johnny-five");
-const board = new Board();
-const accelerometer = new Accelerometer({ controller: "MPU6050" });
 
 const ArduinoBoard = require("./Board.js");
 
-const arduinoBoard = new ArduinoBoard(board, accelerometer);
+const arduinoBoard = new ArduinoBoard(Board, Accelerometer);
+
+setInterval(() => {
+  if (arduinoBoard.imuX > 0.3) {
+    console.log("To Right");
+  } else if (arduinoBoard.imuX < -0.3) {
+    console.log("To Left");
+  } else if (arduinoBoard.imuY > 0.3) {
+    console.log("Forward");
+  } else if (arduinoBoard.imuY < -0.3) {
+    console.log("Backward");
+  }
+}, 1);
